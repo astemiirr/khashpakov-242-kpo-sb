e16c887b216c588886cf76929d81eebf4db18218
astemiirr <astemirhashpakov777@gmail.com>
2025-11-10 23:37:20 +0300

    feat: add HW2 - Banking system with design patterns

--- homework/HW2/folder/hse-bank/src/main/java/ru/hse/bank/command/Command.java
+++
@@ -1,6 +0,0 @@
-package ru.hse.bank.command;
-
-public interface Command {
-    void execute();
-    String getName();
-}
--- homework/HW2/folder/hse-bank/src/main/java/ru/hse/bank/observer/BalanceObserver.java
+++
@@ -1,7 +0,0 @@
-package ru.hse.bank.observer;
-
-import ru.hse.bank.domain.BankAccount;
-
-public interface BalanceObserver {
-    void update(BankAccount account, double oldBalance, double newBalance);
-}
